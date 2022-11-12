d9c23363e667330fc044bb31199680eceb5fd0f8
Islam Magomadov <islam_magomadov@outlook.com>
2022-11-12 21:05:40 +0300

    home02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { faTh, faGear } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'conect';
+  faGear = faGear;
+  faTh = faTh;
 }
